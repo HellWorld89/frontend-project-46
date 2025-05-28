c93cdeda848401c90204b0675cf8e38fc565fa73
Vladimir Sapozhnikov <Sapozhnikovl@yandex.ru>
2025-05-28 19:27:03 +0800

    Добавляет парсинг JSON файлов

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+import { readFileSync } from 'fs';
+import { resolve, extname } from 'path';
+
+const getAbsolutePath = (filepath) => resolve(process.cwd(), filepath);
+const getFileExtension = (filepath) => extname(filepath).slice(1);
+
+const parsers = {
+  json: (content) => JSON.parse(content),
+};
+
 export default function genDiff(filepath1, filepath2, format = 'stylish') {
-  return `Difference between ${filepath1} and ${filepath2} in ${format} format`;
+  
+  const absolutePath1 = getAbsolutePath(filepath1);
+  const content1 = readFileSync(absolutePath1, 'utf-8');
+  const extension1 = getFileExtension(filepath1);
+  const data1 = parsers[extension1](content1);
+
+  const absolutePath2 = getAbsolutePath(filepath2);
+  const content2 = readFileSync(absolutePath2, 'utf-8');
+  const extension2 = getFileExtension(filepath2);
+  const data2 = parsers[extension2](content2);
+
+  return `Comparing:\n${JSON.stringify(data1, null, 2)}\nand\n${JSON.stringify(data2, null, 2)}`;
 }
