b8cc012e15a8cc4b0f9cd40d15fbdbe82a58c879
Vladimir Sapozhnikov <Sapozhnikovl@yandex.ru>
2025-05-28 20:04:53 +0800

    Добавляет сравнение файлов

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/HellWorld89/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/HellWorld89/frontend-project-46/actions)
+
+
+### Asciinema с примером работы вычислителя отличий по пункту 4.
+[![asciicast](https://asciinema.org/a/HUzTFrtTN1TmTTaEr6UgYswtI.svg)](https://asciinema.org/a/HUzTFrtTN1TmTTaEr6UgYswtI)
--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 import { readFileSync } from 'fs';
-import { resolve, extname } from 'path';
+import { resolve } from 'path';
+import _ from 'lodash';
 
 const getAbsolutePath = (filepath) => resolve(process.cwd(), filepath);
-const getFileExtension = (filepath) => extname(filepath).slice(1);
+const parseFile = (filepath) => JSON.parse(readFileSync(getAbsolutePath(filepath), 'utf-8'));
 
-const parsers = {
-  json: (content) => JSON.parse(content),
+const buildDiff = (data1, data2) => {
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
+
+  return keys.map((key) => {
+    if (!_.has(data2, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return [
+        `  - ${key}: ${data1[key]}`,
+        `  + ${key}: ${data2[key]}`
+      ].join('\n');
+    }
+    return `    ${key}: ${data1[key]}`;
+  }).join('\n');
 };
 
-export default function genDiff(filepath1, filepath2, format = 'stylish') {
-  
-  const absolutePath1 = getAbsolutePath(filepath1);
-  const content1 = readFileSync(absolutePath1, 'utf-8');
-  const extension1 = getFileExtension(filepath1);
-  const data1 = parsers[extension1](content1);
-
-  const absolutePath2 = getAbsolutePath(filepath2);
-  const content2 = readFileSync(absolutePath2, 'utf-8');
-  const extension2 = getFileExtension(filepath2);
-  const data2 = parsers[extension2](content2);
-
-  return `Comparing:\n${JSON.stringify(data1, null, 2)}\nand\n${JSON.stringify(data2, null, 2)}`;
+export default function genDiff(filepath1, filepath2) {
+  const data1 = parseFile(filepath1);
+  const data2 = parseFile(filepath2);
+  const diff = buildDiff(data1, data2);
+  return `{\n${diff}\n}`;
 }
