30b5fe6579a75a72e9b7cc0ee1ddedcad3be821b
Vladimir Sapozhnikov <Sapozhnikovl@yandex.ru>
2025-05-28 18:30:53 +0800

    Добавляет аргументы и опции

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,10 +8,11 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2);
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, option) => {
+    const diff = genDiff(filepath1, filepath2, option.format);
     console.log(diff);
   });
 
